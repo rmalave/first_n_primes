d3c8c904ced1ca037cc87298f8b870c2fd199c2c
Rafael Malave <rafomalave@gmail.com>
2015-11-22 12:44:37 -0800

    Refactor

--- first_n_primes.rb
+++ first_n_primes.rb
@@ -1,23 +1,14 @@
-$VERBOSE = nil    
-require 'prime'   
+require 'prime'
 
 def first_n_primes(n)
+  # Check for correct input!
+  "n must be an integer" unless n.is_a? Integer
+  "n must be greater than 0" if n <= 0
 
-  unless n.is_a? Integer
-    return "n must be an integer."
-  end
-
-  if n <= 0
-    return "n must be greater than 0."
-  end
-
-  prime_array = [] if prime_array.nil?
-
-  prime = Prime.new
-  for num in (1..n)
-    prime_array.push(prime.next)
-  end
-  return prime_array
+  # The Ruby 1.9 Prime class makes the array automatically!
+  prime = Prime.instance
+  prime.first n
 end
 
 first_n_primes(10)
+
